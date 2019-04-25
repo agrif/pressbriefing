a420d53a855144698836607626c275b0951f297a
Aaron Griffith <aargri@gmail.com>
2019-04-24 21:28:02 -0400

    update style, reorganize informatios

--- src/pressbriefing.js
+++ src/pressbriefing.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
+function updateClock() {
     var now = new Date();
     var then = Date.parse(info.date);
     var diff = (now.getTime() - then) / 1000;
     var clock = $('.clock').FlipClock(diff, {
         clockFace: 'DailyCounter',
+        showSeconds: false,
     });
+    $('.latest').html(
+        '<a href="' + info['url'] + '">' + info['title'] + '</a>'
+    );
+}
+
+$(document).ready(function() {
+    updateClock();
 });
