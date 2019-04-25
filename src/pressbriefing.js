da23497416859133525f2e0111d6e9db7e58d5a5
Aaron Griffith <aargri@gmail.com>
2019-04-24 21:18:04 -0400

    moved files around, added update.py script

--- src/pressbriefing.js
+++ src/pressbriefing.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
     var now = new Date();
-    // month is... 0-indexed?? ? ?
-    var then = new Date(2019, 2, 11);
-    var diff = (now.getTime() - then.getTime()) / 1000;
+    var then = Date.parse(info.date);
+    var diff = (now.getTime() - then) / 1000;
     var clock = $('.clock').FlipClock(diff, {
         clockFace: 'DailyCounter',
     });
---
+++ update.py
@@ -0,0 +1,84 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+import json
+import shutil
+import datetime
+import os.path
+import os
+import sys
+
+url = 'https://www.youtube.com/feeds/videos.xml?playlist_id=PLRJNAhZxtqH_3Nl-7n1vhgTUHyFSuQ0nI'
+datefmt = '%Y-%m-%dT%H:%M:%S+00:00'
+destroot = "wwwroot"
+
+ns = {
+    'atom': 'http://www.w3.org/2005/Atom',
+    'media': 'http://search.yahoo.com/mrss/',
+    'yt': 'http://www.youtube.com/xml/schemas/2015',
+}
+
+def get_latest():
+    with urllib.request.urlopen(url) as f:
+        data = ET.fromstring(f.read().decode('utf-8'))
+
+    infos = []
+    for entry in data.findall('atom:entry', ns):
+        datestr = entry.find('atom:published', ns).text
+        date = datetime.datetime.strptime(datestr, datefmt)
+        date = date.replace(tzinfo=datetime.timezone.utc)
+        info = {
+            'date': date,
+            'title': entry.find('atom:title', ns).text,
+            'url': entry.find('atom:link', ns).attrib['href'],
+        }
+        infos.append(info)
+
+    infos.sort(key=lambda d: d['date'], reverse=True)
+    info = infos[0]
+    return info
+
+def move(src):
+    _, head = os.path.split(src)
+    truedest = os.path.join(destroot, head)
+    try:
+        dest_mtime = os.stat(truedest).st_mtime
+    except FileNotFoundError:
+        dest_mtime = 0
+    if os.stat(src).st_mtime - dest_mtime > 0:
+        print(src, '->', truedest)
+        shutil.copy2(src, destroot)
+
+def generate(name):
+    truedest = os.path.join(destroot, name)
+    print('generating', truedest)
+    return open(truedest, 'w')
+
+def main():
+    # make sure we are working where we think we are
+    base, _ = os.path.split(os.path.abspath(__file__))
+    os.chdir(base)
+
+    fetch = not '-n' in sys.argv
+
+    if fetch:
+        print('fetching rss...')
+        info = get_latest()
+        local = info['date'].astimezone()
+        print('last conference was', local.strftime("%a %b %d, %H:%M %Z"))
+
+    move("src/index.html")
+    move("src/pressbriefing.js")
+    move("src/pressbriefing.css")
+    move("extern/jquery-3.4.0.min.js")
+    move("extern/flipclock/compiled/flipclock.min.js")
+    move("extern/flipclock/compiled/flipclock.css")
+
+    if fetch:
+        info['date'] = info['date'].strftime(datefmt)
+        with generate('start.js') as f:
+            f.write('var info = {};\n'.format(json.dumps(info)))
+        with generate('update.json') as f:
+            f.write('{}\n'.format(json.dumps(info)))
+
+if __name__ == "__main__":
+    main()
